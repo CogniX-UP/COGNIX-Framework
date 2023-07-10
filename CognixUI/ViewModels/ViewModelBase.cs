5e446dd3840e524280baf5759be7d03102ce8736
Papadou <salokinsomisa2@gmail.com>
2023-07-10 08:08:29 +0300

    Cognix UI Project added

---
+++ CognixUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CognixUI.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ CognixUI/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace CognixUI.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
